a4031c4348b2fec5d5da331a32c9769a042be42a
SAULTULA <67132634+SAULTULA@users.noreply.github.com>
2026-08-16 00:04:34 -0300

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  root: __dirname,
   plugins: [react()],
   build: {
     outDir: 'dist',
